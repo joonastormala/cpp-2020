b321dad22703561bd353d9b1cb0935bd85a7e8c9
Joonas Tormala <joonastorm@gmail.com>
2020-06-04 17:51:00 +0300

    vector

--- Module_1/vector/src/vector.cpp
+++ Module_1/vector/src/vector.cpp
@@ -3,16 +3,27 @@
 #include <vector>
 
 int GetMin(std::vector<int> v) {
-    
-    return 0; 
+    int min = 1000;
+    for(unsigned int i = 0; i < v.size(); i++){
+        if(v[i] < min) min = v[i];
+    }
+    return min; 
 }
 
 int GetMax(std::vector<int> v) {
-    
-    return 0; 
+    int max = -1000;
+    for(unsigned int i = 0; i < v.size(); i++){
+        if(v[i] > max) max = v[i];
+    }
+    return max; 
 }
 
 double GetAvg(std::vector<int> v) {
-    
-    return 0.0; 
+    double numbers = 0;
+    unsigned int i = 0;
+    for(; i < v.size(); i++){
+        numbers += v[i];
+    }
+    numbers = numbers/i;
+    return numbers; 
 }
