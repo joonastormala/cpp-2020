ce4d5ff4cbe3c9422e8c248f81dfe4cb999aeee8
Joonas Tormala <joonastorm@gmail.com>
2020-06-10 16:22:51 +0300

    iterator

--- Module_2/vector_iterators/src/vector_it.cpp
+++ Module_2/vector_iterators/src/vector_it.cpp
@@ -2,6 +2,29 @@
 #include <vector>
 #include "vector_it.hpp"
 
-// Implement the functions here
+using namespace std;
 
+vector<int> ReadVector(){
+    vector<int> v;
+    int input;
+    cin >> input;
+    do {
+        v.push_back(input);
+    } while(cin >> input);
+    return v;
+}
 
+void PrintSum1(const vector<int>&v){
+    for(unsigned int i = 0; i<v.size()-1; i++){
+        cout << (v[i] + v[i+1]) << " ";
+    }
+    cout << endl;
+}
+
+void PrintSum2(const vector<int>&v){
+    unsigned int j = v.size() - 1;
+    for(unsigned int i = 0; i<v.size()/2;i++,j--){
+            cout << v[i]+v[j] << " ";
+    }
+    cout << endl;
+}
