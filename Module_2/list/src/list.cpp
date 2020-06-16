0c29b397e73ef45795bb9b3c5bffe02a0fc74120
Joonas Tormala <joonastorm@gmail.com>
2020-06-16 12:39:53 +0300

    list

--- Module_2/list/src/list.cpp
+++ Module_2/list/src/list.cpp
@@ -1,5 +1,23 @@
 #include "list.hpp"
 #include <iostream>
 
-// Implement the functions here
+using namespace std;
 
+istream& GetLines(istream& is, list<string>& list){
+    string str;
+    while(getline(is, str)){
+        if(str.size()>0) list.push_back(str);
+    }
+    return is;
+}
+
+void Print(const list<string>& list){
+    for(auto i : list){
+        cout << i << endl;
+    }
+}
+
+void SortAndUnique(list<string>& list){
+    list.unique();
+    list.sort();
+}
