4a30c0390d8d1d230a161e6a5ad6c0e58bc13096
Joonas Tormala <joonastorm@gmail.com>
2020-06-16 20:27:29 +0300

    matrix1

--- Module_2/matrix/src/matrix.cpp
+++ Module_2/matrix/src/matrix.cpp
@@ -11,12 +11,12 @@ Matrix ReadMatrix(int n){
         std::cin >> j;
         return m;
     }
+    else return Matrix();
 }
 
 Matrix Rotate90Deg(const Matrix &m){
     int row = m.size();
-    int col = m[0].size();
-    Matrix r(row, std::vector<int> (col));
+    Matrix r(row, std::vector<int> (row));
     for(int i = 0; i<row;i++)
     for(int j = 0; j<row;j++)
     r[i][j] = m[row-j-1][i];
@@ -24,6 +24,8 @@ Matrix Rotate90Deg(const Matrix &m){
 }
 
 void Print(const Matrix &m){
+    int len = m.size();
+    std::cout << "Printing out a " << len <<" x " << len << " matrix:" << std::endl;
     for(unsigned int i = 0; i < m.size(); i++){
         for(unsigned int j = 0; j < m[i].size(); j++){
             std::cout << m[i][j] << " ";
