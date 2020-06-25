1341117d64f706ef30c73d44d9a6cc5484777243
Joonas Tormala <joonastorm@gmail.com>
2020-06-26 02:04:10 +0300

    dragons2

--- Module_3/dragons/src/magic_dragon.cpp
+++ Module_3/dragons/src/magic_dragon.cpp
@@ -4,22 +4,22 @@ MagicDragon::MagicDragon(const std::string& name, size_t age, size_t size)
             : Dragon(name, age, size) {}
 
 void MagicDragon::Eat(std::list<Food>& f){
-    for(std::list<Food>::iterator i = f.begin(); i != f.end(); i++){
+    for(std::list<Food>::iterator i = f.begin(); i != f.end();){
         if(i->type == Herbs) {
             size_++;
             std::cout << "Magic dragon ate: " << i->name << std::endl;
             i = f.erase(i);
-        }
+        } else i++;
     }
 }
 
 void MagicDragon::Hoard(std::list<Treasure>& t){
-    for(std::list<Treasure>::iterator i = t.begin(); i != t.end(); i++){
+    for(std::list<Treasure>::iterator i = t.begin(); i != t.end();){
         if(i->type == Potions) {
             treasures_.push_back(*i);
             std::cout << "Magic dragon recieved: " << i->name << std::endl;
             i = t.erase(i);
-        }
+        } else i++;
     }
     // std::list<Treasure>::iterator i = t.begin();
     // while(i != t.end()){
